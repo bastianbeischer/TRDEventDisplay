08cf5100fefaa1358513d8e776871ac52cff70af
beischer <beischer>
2010-08-17 19:08:12 +0000

    *** empty log message ***

--- main.cc
+++ main.cc
@@ -9,11 +9,14 @@ int main(int argc, char** argv)
   window->show();
 
   // in case of command line arguments: open the respective files
+
+  // directly...
   QStringList args = app.arguments();
   if (args.size() == 2) {
     window->openFile(args.at(1));
   }
 
+  // by XXXX YYY format from AMS_ROOTFILE_DIR directory
   if (args.size() == 3) {
     QString dir = window->amsRootFileDir();
     if (dir != "") {

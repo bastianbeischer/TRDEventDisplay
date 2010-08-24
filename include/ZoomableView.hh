5dea57391d61f386bec14f7cff1921aa68ea0a64
beischer <beischer>
2010-08-24 15:42:11 +0000

    *** empty log message ***

--- include/ZoomableView.hh
+++ include/ZoomableView.hh
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////
 // CVS Information
-// $Id: ZoomableView.hh,v 1.2 2010/08/18 19:36:43 beischer Exp $
+// $Id: ZoomableView.hh,v 1.3 2010/08/24 15:42:11 beischer Exp $
 /////////////////////////////////////////////////////////////////
 
 #ifndef ZoomableView_hh
@@ -8,6 +8,13 @@
 
 #include <QGraphicsView>
 
+/**
+ * @class ZoomableView
+ *
+ * @brief QGraphicsView with zooming capabilities
+ * 
+ * This class is a simple QGraphicsView which overrides the default mouse events in QGraphicsView to create zooming functionality.
+ */
 class ZoomableView :
   public QGraphicsView
 {
@@ -15,25 +22,63 @@ class ZoomableView :
 Q_OBJECT
 
 public:
+  
+  /**
+   * @brief Constructor, sets the members to default values. The left-click-and-drag behavior is set to "scroll" the associated scene.
+   * @param parent Parent widget (if any)
+   */
   ZoomableView(QWidget* parent = 0);
+
+  /**
+   * @brief Destructor, deletes the zoom point and rectangle if they still exist
+   */
   ~ZoomableView();
   
 public slots:
+
+  /**
+   * @brief Zoom in by a constant factor
+   */
   void zoomIn()   {scale(m_zoomFactor,    m_zoomFactor);}
+
+  /**
+   * @brief Zoom out by a constant factor
+   */
   void zoomOut()  {scale(1./m_zoomFactor, 1./m_zoomFactor);}
+
+  /**
+   * @brief Fit the scene into the view.
+   */
   void fitScene() {fitInView(sceneRect());}
 
 protected:
+
+  /**
+   * @brief If the user pressed the right or middle mouse button, take corresponding actions.
+   */
   void mousePressEvent(QMouseEvent* mouseEvent);
+
+  /**
+   * @brief If the user releases the right mouse button, zoom in to the specified rectangle.
+   */
   void mouseReleaseEvent(QMouseEvent* mouseEvent);
+
+  /**
+   * @brief Update the pictural representation of the zooming rectangle in the scene.
+   */
   void mouseMoveEvent(QMouseEvent* mouseEvent);
+
+  /**
+   * @brief Wheel up zooms in, wheel down zooms out.
+   */
   void wheelEvent(QWheelEvent* event);
   
 private:
-  double             m_zoomFactor;
 
-  QPointF*           m_mousePressedAt;
-  QGraphicsRectItem* m_zoomRectangle;
+  double             m_zoomFactor;      /**< Factor by which the scene should be zoomed when using the mouse wheel. */
+
+  QPointF*           m_mousePressedAt;  /**< Point at which the mouse was pressed for rectangle calculation. */
+  QGraphicsRectItem* m_zoomRectangle;   /**< The rectangle which represents the region to zoom into. */
 
 };
 
